b0fabab7df4828dd7b6a7582bd782ec3abd089ac
Ayman Dandan <aymandandan24680@gmail.com>
2025-11-25 10:15:05 +0200

    Added extended entities

---
+++ src/unimade.MTPortal.Domain/Tenants/AppTenant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.TenantManagement;
+
+namespace unimade.MTPortal.Tenants
+{
+    public class AppTenant : Tenant
+    {
+        public string Country { get; set; }
+        public string ContactEmail { get; set; }
+        public string DisplayName { get; set; }
+
+        protected AppTenant() { } // For ORM
+
+        public AppTenant(
+            Guid id,
+            string name,
+            string country,
+            string contactEmail,
+            string displayName)
+            : base(
+                  id,
+                  name,
+                  null)
+        {
+            Country = country;
+            ContactEmail = contactEmail;
+            DisplayName = displayName;
+        }
+
+    }
+}
---
+++ src/unimade.MTPortal.Domain/Users/AppUser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Identity;
+
+namespace unimade.MTPortal.Users
+{
+    public class AppUser : IdentityUser
+    {
+        public UserType UserType { get; set; }
+
+        protected AppUser() { } // For ORM
+
+        public AppUser(
+            Guid id,
+            string userName,
+            string email,
+            Guid? tenantId = null)
+            : base(
+                  id,
+                  userName,
+                  email,
+                  tenantId)
+        {
+            UserType = UserType.Public;
+        }
+
+        public bool IsStaffUser() => UserType == UserType.Staff;
+
+        public bool IsPublicUser() => UserType == UserType.Public;
+    }
+}
